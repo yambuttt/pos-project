287680cb97e91c24afbb834545daf2f347d72cf4
yambuttt <hanyagabuttt0@gmail.com>
2026-04-05 18:39:24 +0700

    Add ReservationItem model

---
+++ app/Models/ReservationItem.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReservationItem extends Model
+{
+    protected $fillable = [
+        'reservation_id','product_id','qty','price_snapshot','note'
+    ];
+
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
